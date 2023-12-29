b565f214caf35e5a70e39678309dd1478c8bc8ad
Afridi <afridiwhatever@gmail.com>
2023-12-30 04:02:59 +0600

    Preloader complete

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+"use strict";
 
+const preloader = document.querySelector(".preload");
+
+window.addEventListener("load", function () {
+  setTimeout(() => {
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+  }, 1800);
+});
