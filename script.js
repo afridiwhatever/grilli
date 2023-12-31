42645da87ca6e3e2e0507e2f744b8b4cb1832ce5
Afridi <afridiwhatever@gmail.com>
2023-12-31 19:02:20 +0600

    mobile navbar complete with functionality

--- script.js
+++ script.js
@@ -6,5 +6,24 @@ window.addEventListener("load", function () {
   setTimeout(() => {
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
-  }, 1000);
+  }, 300);
 });
+
+// add click events for toggling navbar
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+
+const addEventsOnElement = (elements, eventType, callback) => {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+addEventsOnElement(navTogglers, "click", toggleNavbar);
